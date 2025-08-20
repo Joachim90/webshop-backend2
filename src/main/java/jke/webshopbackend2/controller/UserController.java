abe9a5a6c8875c65b3a02598166019221a9c0e5c
Joachim Wiltman <jke@live.se>
2025-08-20 11:37:47 +0200

    project startup

--- src/main/java/jke/webshopbackend2/controller/UserController.java
+++ src/main/java/jke/webshopbackend2/controller/UserController.java
@@ -1,16 +1,5 @@
 package jke.webshopbackend2.controller;
-
-import jakarta.servlet.http.HttpSession;
-import jke.webshopbackend2.dto.LoginRequest;
-import jke.webshopbackend2.dto.RegisterRequest;
-import jke.webshopbackend2.dto.UserDto;
-import jke.webshopbackend2.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class UserController {
