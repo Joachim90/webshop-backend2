79e04479646b629527251134b96ad73946243dbe
Isak Folke <isvack@gmail.com>
2025-08-25 13:00:11 +0200

    Fixade tabellerna så att de skapas ordentligt

--- src/main/java/jke/webshopbackend2/model/CustomerOrder.java
+++ src/main/java/jke/webshopbackend2/model/CustomerOrder.java
@@ -3,8 +3,6 @@ package jke.webshopbackend2.model;
 
 import jakarta.persistence.*;
 
-
-
 @Entity
 public class CustomerOrder {
     public CustomerOrder(User customer, Product product) {
@@ -13,15 +11,13 @@ public class CustomerOrder {
     }
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public int id;
 
     @ManyToOne
-    @JoinColumn(name = "user_id")
     private User customer;
 
     @ManyToOne
-    @JoinColumn(name = "product_id")
     private Product product;
 
     public CustomerOrder() {
