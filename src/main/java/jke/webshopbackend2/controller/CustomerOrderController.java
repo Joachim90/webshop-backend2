eba1b7852564bafba96b32a1fbcbb462e28fc14b
Joachim Wiltman <jke@live.se>
2025-08-25 09:03:43 +0200

    Påbörjat fixa med kundbeställningar, ej klar.

--- src/main/java/jke/webshopbackend2/controller/CustomerOrderController.java
+++ src/main/java/jke/webshopbackend2/controller/CustomerOrderController.java
@@ -2,6 +2,7 @@ package jke.webshopbackend2.controller;
 
 import jakarta.servlet.http.HttpSession;
 import jke.webshopbackend2.dto.UserDto;
+import jke.webshopbackend2.model.User;
 import jke.webshopbackend2.service.CustomerOrderService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,8 +20,8 @@ public class CustomerOrderController {
 
     @PostMapping("/purchase")
     public String purchaseProduct(@RequestParam("productId") int productId, HttpSession session, RedirectAttributes redirectAttributes) {
-        UserDto user = (UserDto) session.getAttribute("user");
-
+        //UserDto user = (UserDto) session.getAttribute("user");
+        User user = (User) session.getAttribute("user");
         final String result = customerOrderService.purchaseProduct(productId, user);
 
         return "shop";
