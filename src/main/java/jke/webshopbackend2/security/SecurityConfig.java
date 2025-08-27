554c937d67a568666bdc7fc94cfe30b9db57bd71
Jesper Ekstedt <jemaek@gmail.com>
2025-08-27 12:35:42 +0200

    Integrationstester

--- src/main/java/jke/webshopbackend2/security/SecurityConfig.java
+++ src/main/java/jke/webshopbackend2/security/SecurityConfig.java
@@ -35,13 +35,4 @@ public class SecurityConfig {
         return http.build();
     }
 
-    /* @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .formLogin().disable()
-                .csrf(csrf -> csrf.disable());
-
-        return http.build();
-    }*/
 }
